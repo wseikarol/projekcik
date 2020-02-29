723baaf4455844ad96a2b8d45bfd30de0c6598b4
unknown <karol386@interia.pl>
2020-02-29 15:52:54 +0100

    Task-0002 - jakas tam funkcja dodajaca

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>// zad 1
 using namespace std;
+
+float dodajLiczby(float a, float b) {
+    return a + b;
+}
 int main() {
 
     cout << "Hello world" << endl;
+
+    float a, b;
+    cout << "Podaj 2 liczby do dodania" << endl;
+    cin >> a >> b;
+
+    cout << "Wynik dodawania: " << dodajLiczby(a,b) << endl;
     return 0;
 }
 
