a7937a02662d623d3d302689d59106747bacdac1
unknown <karol386@interia.pl>
2020-02-29 15:34:36 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/projekt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt.dir/main.cpp.o"
+  "libprojekt.dll.a"
+  "projekt.exe"
+  "projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>// zad 1
+using namespace std;
+int main() {
+
+    cout << "Hello world" << endl;
+    return 0;
+}
+
