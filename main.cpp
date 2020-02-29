09dcb5ab86deb2f6e744eae0d356fd13ec8cc45b
unknown <karol386@interia.pl>
2020-02-29 15:44:04 +0100

    Task-0001 - funkcja jakastam

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>// zad 1
 using namespace std;
+
+int porownajLiczby(int a, int b) {
+    return a > b ? a : b;
+}
 int main() {
 
     cout << "Hello world" << endl;
+    int a,b;
+
+    cout << "Podaj dwie liczby!" << endl;
+    cin >> a >> b;
+
+    cout << "Większa liczba to: " << porownajLiczby(a,b) << endl;
     return 0;
 }
 
